e96fc53a7aad5e859667a451705e5c673500f5a5
4frag <alexmainacc0unt@gmail.com>
2025-07-26 22:23:33 +0300

    .

--- restore-postgres.sh
+++ restore-postgres.sh
@@ -1,2 +1,2 @@
 docker cp ./test_task2.dump test_task2-postgres:/
-docker exec test_task2-postgres pg_restore -d test_task2 test_task2.dump
+docker exec test_task2-postgres pg_restore -U test_task2 -d test_task2 test_task2.dump
